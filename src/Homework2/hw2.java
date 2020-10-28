737645f8af5506f4041026d7b950d0cab9af9dee
Dragon757 <dragon40in@gmail.com>
2020-10-28 22:35:37 +0300

    test2

---
+++ src/Homework2/hw2.java
@@ -0,0 +1,28 @@
+package Homework2;
+
+public class hw2 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[8];
+        arr[0] = 1;
+        for (int i = 0, y =0; y < arr.length; y++) {
+            System.out.println(arr[i]);
+            arr[i] += 3;
+        }
+    }
+}
+ //Задание 1
+//    public static void invertArray() {
+//        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+//        for (int i = 0; i < arr.length; i++){
+//            if (arr[i] == 1) {
+//                arr[i] = 0;
+//            } else {
+//                arr[i] = 1;
+//            }
+//        }
+//    }
+ //Задание 2
+
+
+
