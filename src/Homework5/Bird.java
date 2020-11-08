65f161f927f3fa39ee6e7808ac58fad0a19548a4
Dragon757 <dragon40in@gmail.com>
2020-11-08 19:20:46 +0300

    11

---
+++ src/Homework5/Bird.java
@@ -0,0 +1,7 @@
+package Homework5;
+
+public class Bird extends Animals{
+    Bird(String name, float maxJump, float maxRun, float maxSwim) {
+        super("Птица", name, maxJump, maxRun, maxSwim);
+    }
+}
---
+++ src/Homework5/Dog.java
@@ -0,0 +1,7 @@
+package Homework5;
+
+public class Dog extends Animals{
+    Dog(String name, float maxJump, float maxRun, float maxSwim) {
+        super("Пёс", name, maxJump, maxRun, maxSwim);
+    }
+}
