e8c2a689bcba3c545db2f48ef9c0bdf1b791a2c4
40inDragon <73475713+40inDragon@users.noreply.github.com>
2020-11-16 18:26:12 +0300

    Delete Dog.java

--- src/Homework2/Dog.java
+++
@@ -1,7 +0,0 @@
-package Homework2;
-
-public class Dog extends Animals{
-    Dog(String name, float maxJump, float maxRun, float maxSwim) {
-        super("Пёс", name, maxJump, maxRun, maxSwim);
-    }
-}
