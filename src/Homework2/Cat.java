4b8dd4ce8d93ca96f71f4a1cfa920492f0f0195f
40inDragon <73475713+40inDragon@users.noreply.github.com>
2020-11-16 18:25:44 +0300

    Delete Cat.java

--- src/Homework2/Cat.java
+++
@@ -1,9 +0,0 @@
-package Homework2;
-
-public class Cat extends Animals{
-
-    Cat(String name, float maxJump, float maxRun, float maxSwim) {
-        super("Кот", name, maxJump, maxRun, maxSwim);
-    }
-
-}
