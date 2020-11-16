7a024d6f73c3f6e095ab6c955ca9ceeba3c82a1e
Dragon757 <dragon40in@gmail.com>
2020-11-16 18:29:22 +0300

    123

---
+++ src/hw7/Game.java
@@ -0,0 +1,10 @@
+package hw7;
+
+import Homework2.MainWindow;
+
+public class Game {
+    public static void main(String[] args) {
+        new MainWindow();
+        System.out.println("method main is end");
+    }
+}
