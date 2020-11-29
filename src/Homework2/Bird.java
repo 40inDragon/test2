9de0dddbf85b50368e308fe6269476dfa027cd4d
40inDragon <73475713+40inDragon@users.noreply.github.com>
2020-11-29 15:40:58 +0300

    Delete Bird.java

--- src/Homework2/Bird.java
+++
@@ -1,7 +0,0 @@
-package Homework2;
-
-public class Bird extends Animals{
-    Bird(String name, float maxJump, float maxRun, float maxSwim) {
-        super("Птица", name, maxJump, maxRun, maxSwim);
-    }
-}
